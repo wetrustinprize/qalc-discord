f92b3a11424c17d90da0f43a68799d900e5d6dc6
wetrustinprize <me@wetrustinprize.com>
2026-03-03 17:22:13 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+a simple poise[^1] discord bot app for calculating stuff with libqalculate[^2]
+
+---
+
+### usage
+
+the usage is simple, just add the [bot](https://discord.com/oauth2/authorize?client_id=1478034805236043828) into your discord apps and run it with the following command:
+
+```
+/calc 1 + 5
+```
+
+it will solve almost any formula, take a look at the library to know more about libqalculate[^2] features
+
+[^1] https://github.com/serenity-rs/poise
+[^2] https://github.com/Qalculate/libqalculate
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "qalc Discord bot dev shell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            pkg-config
+            rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            }
+          ];
+        };
+      }
+    );
+}
