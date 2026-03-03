f1d724853e71e8a133a93315045e62532de7d061
wetrustinprize <me@wetrustinprize.com>
2026-03-03 19:21:44 -0300

    refactor: changed how qalculate is called

---
+++ .changeset/changed_how_the_qalculate_is_called.md
@@ -0,0 +1,5 @@
+---
+default: minor
+---
+
+# changed how the qalculate is called
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,62 @@
 use poise::serenity_prelude as serenity;
-use qalculate_rs::Qalculate;
+use std::time::Duration;
+use tokio::process::Command;
+use tokio_process_tools::*;
 
 struct Data {}
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-#[poise::command(slash_command, prefix_command)]
-async fn calc(
-    ctx: Context<'_>,
-    #[description = "Formula"] formula: String,
-) -> Result<(), Error> {
-    let formula_clone = formula.clone();
-    let response = tokio::task::spawn_blocking(move || {
-        let calc = Qalculate::new().unwrap();
-        calc.calculate_string(&formula_clone).unwrap()
-    })
-    .await?;
+/// Calculates equations using libqalculate
+///
+/// Examples:
+/// /calc 2 + 2
+/// /calc 2days hours * 3
+/// /calc 20hours to seconds
+#[poise::command(slash_command)]
+async fn calc(ctx: Context<'_>, #[description = "Formula"] formula: String) -> Result<(), Error> {
+    println!("Received a request from {} to calculate `{}`", ctx.author().name, formula);
 
-    ctx.say(format!("`{}` = **{}**", formula, response)).await?;
-    Ok(())
+    let mut cmd = Command::new("qalc");
+    cmd.arg(&formula);
+
+    let mut proc = Process::new(cmd).spawn_single_subscriber().unwrap();
+
+    match ctx.defer().await {
+        Ok(_) => {},
+        Err(e) => {
+            println!("An error occoured when defering the context: {}", e);
+            return Ok(());
+        }
+    }
+
+    let result = proc
+        .wait_for_completion_with_output_or_terminate(
+            Duration::from_secs(2),
+            Duration::from_secs(1),
+            Duration::from_secs(1),
+            LineParsingOptions::default(),
+        )
+        .await;
+
+    match result {
+        Ok(output) => {
+            if output.status.success() {
+                let message = output.stdout.join("");
+                ctx.say(message).await?;
+            } else {
+                println!("Failed to wait for qalc result: `{}`, took too long.", formula);
+                ctx.say("This operation took too long! We canceled the calculation. ;)").await?;
+            }
+
+            Ok(())
+        }
+        Err(err) => {
+            println!("Failed to wait for qalc result: `{}`: {}", formula, err);
+            ctx.say("There was an internal error while calculating the result, please try again later").await?;
+            Ok(())
+        }
+    }
 }
 
 #[tokio::main]
